6d94e210373270309e20677239c6882edc9e0a86
Duc111095 <huynhduc111095@gmail.com>
2025-01-09 01:48:24 +0700

    09.01 commit

--- src/main/java/com/ducnh/chandanaoe/database/seeder/DatabaseSeeder.java
+++ src/main/java/com/ducnh/chandanaoe/database/seeder/DatabaseSeeder.java
@@ -1,13 +1,40 @@
 package com.ducnh.chandanaoe.database.seeder;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Something seeding");
+        if (isTableEmpty()) {
+            String passwordEncode = passwordEncoder.encode("password");
+            entityManager.createNativeQuery("INSERT INTO users (name, email, password, user_catalogue_id, phone) VALUES (?, ?, ?, ?, ?)")
+                            .setParameter(1, "Duc Nguyen")
+                            .setParameter(2, "huynhduc111095@gmail.com")
+                            .setParameter(3, passwordEncode)
+                            .setParameter(4, 1)
+                            .setParameter(5, "0377776595")
+                            .executeUpdate();
+        }
     }
-}
+
+    private boolean isTableEmpty() {
+        Long count = (Long) entityManager.createQuery("SELECT count(id) FROM User").getSingleResult();
+        return count == 0;
+    }
+;}
--- src/main/java/com/ducnh/chandanaoe/modules/users/entities/User.java
+++ src/main/java/com/ducnh/chandanaoe/modules/users/entities/User.java
@@ -1,4 +1,112 @@
 package com.ducnh.chandanaoe.modules.users.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_catalogue_id")
+    private Long userCatalogueId;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String image;
+    private String address;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreated() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdated() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserCatalogueId() {
+        return this.userCatalogueId;
+    }
+
+    public void setUserCatalogueId(Long userCatalogueId) {
+        this.userCatalogueId = userCatalogueId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return this.createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
+    }
 }
